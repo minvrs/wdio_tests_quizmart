47b6895d5166e2fc2221cb7692156a86b7ec831d
Mindaugas <>
2022-01-22 22:44:16 +0200

    changes to login_page and working old tests

--- web/tests/login_with_email_tests.ts
+++ web/tests/login_with_email_tests.ts
@@ -1,13 +1,12 @@
-import * as landingPage from "../pageObjects/landing_page"
 import * as defaultPage from "../pageObjects/default_page"
 import * as loginPage from '../pageObjects/login_page'
+import * as userCredentials from '../utils/user_credentials'
 import { expect } from "chai"
+import { validationFieldMessages } from '../utils/validation_field_messages'
 
-
-const expectedIncorrectEmailOrPassMsg = 'The email address or password is incorrect'
-const expectedEmailOrPassRequireMsg = 'Required'
-const expectedInvalidEmailMsg = 'Must be valid email'
-const expectedShortPassMsg = 'Password must be at least of 6 characters in length'
+const incorectPass = 'ecret123'
+const invalidEmail = 'qwerty'
+const shortPass = '1234'
 
 
 describe('Login with email tests', () => {
@@ -18,34 +17,32 @@ describe('Login with email tests', () => {
     })
 
     it('Inccorect email or password message', async () => {
-        await loginPage.enterEmail()
-        await loginPage.enterPass()
+        await loginPage.signInWithEmail(userCredentials.user.email, incorectPass)
+        browser.pause(2000)
         await loginPage.clickSignInBtn()
-        expect(await loginPage.getIncorrectEmailOrPassMsg()).equals(expectedIncorrectEmailOrPassMsg)
+        expect(await loginPage.getIncorrectEmailOrPassMsg()).equals(validationFieldMessages.incorrectEmailOrPassMsg)
 
     })
 
     it('Email and Password required message', async () => {
         browser.refresh()
         await loginPage.clickSignInBtn()
-        expect(await loginPage.getInvalidEmailMsg()).equals(expectedEmailOrPassRequireMsg)
-        expect(await loginPage.getInvalidPassdMsg()).equals(expectedEmailOrPassRequireMsg)
+        expect(await loginPage.getInvalidEmailMsg()).equals(validationFieldMessages.RequireMsg)
+        expect(await loginPage.getInvalidPassdMsg()).equals(validationFieldMessages.RequireMsg)
 
     })
 
     it('Invalid Email message', async () => {
         browser.refresh()
-        await loginPage.enterInvalidEmail()
-        await loginPage.clickSignInBtn()
-        expect(await loginPage.getInvalidEmailMsg()).equals(expectedInvalidEmailMsg)
+        await loginPage.enterEmailAndClickPassField(invalidEmail)
+        expect(await loginPage.getInvalidEmailMsg()).equals(validationFieldMessages.invalidEmailMsg)
 
     })
 
     it('Short Password message', async () => {
         browser.refresh()
-        await loginPage.enterShortPass()
-        await loginPage.clickSignInBtn()
-        expect(await loginPage.getInvalidPassdMsg()).equals(expectedShortPassMsg)
+        await loginPage.enterPassAndClickEmailField(shortPass)
+        expect(await loginPage.getInvalidPassdMsg()).equals(validationFieldMessages.shortPassMsg)
 
     })
 
--- web/utils/validation_field_messages.ts
+++ web/utils/validation_field_messages.ts
@@ -1,6 +1,6 @@
 export enum validationFieldMessages {
     incorrectEmailOrPassMsg = 'The email address or password is incorrect',
-    emailOrPassRequireMsg = 'Required',
+    RequireMsg = 'Required',
     invalidEmailMsg = 'Must be valid email',
     shortPassMsg = 'Password must be at least of 6 characters in length',
 }
