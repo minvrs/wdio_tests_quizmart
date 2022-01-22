026150db6d27f91bb4758113f69bd629ddb0f5ce
Mindaugas <>
2022-01-22 21:23:37 +0200

    created utils folder

---
+++ web/utils/user_credentials.ts
@@ -0,0 +1,4 @@
+export const user = {
+    email: 'mindaugas.verseckas+quiz@telesoftas.com',
+    password: 'Secret123',
+}
---
+++ web/utils/validation_field_messages.ts
@@ -0,0 +1,6 @@
+export enum validationFieldMessages {
+    incorrectEmailOrPassMsg = 'The email address or password is incorrect',
+    emailOrPassRequireMsg = 'Required',
+    invalidEmailMsg = 'Must be valid email',
+    shortPassMsg = 'Password must be at least of 6 characters in length',
+}
